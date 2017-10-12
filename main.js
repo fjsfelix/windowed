627282b33ef8c74318e3b3f6b3b3a6e4caff6f17
Felix <fjs.felix.sun@gmail.com>
2017-10-12 19:24:05 -0400

    added menu copy/paste mac

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const analytics = new Analytics.default('UA-64857602-3');
 
 const electron = require('electron')
 const app = electron.app
+const Menu = electron.Menu
 const BrowserWindow = electron.BrowserWindow
 
 
@@ -21,6 +22,20 @@ function createWindow () {
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+  var template = [{
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 function googleAnalytics(){
